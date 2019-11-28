92797f70c956b40d4b7ce3561958aafd6a052307
X-bear <522168288@qq.com>
2019-11-28 14:58:23 +0800

    init drag

---
+++ 005 drag/src/components/Survey.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+
+</template>
+<script>
+import SurveyList from './SurveyList';
+import SurveyOption from './SurveyOption';
+import SurveyType from './SurveyType';
+export default {
+  data() {
+    return {};
+  },
+  computed: { SurveyList, SurveyOption, SurveyType }
+};
+</script>
+<style lang="scss" scoped>
+</style>
